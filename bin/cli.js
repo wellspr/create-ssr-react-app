a35656c145abf0e2ff10d0fa79ce05dfd0d961d4
Paulo <paulowells@gmail.com>
2022-04-03 17:44:26 -0300

    added build step to cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -16,6 +16,7 @@ const runCommand = command => {
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/wellspr/create-ssr-react-app ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
+const buildCommand = `cd ${repoName} && npm run build`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
@@ -24,5 +25,9 @@ if (!checkedOut) process.exitCode = -1;
 console.log(`Installing dependencies for ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exitCode = -1;
+
+console.log(`Building development server.`);
+const builtProject = runCommand(buildCommand);
+if (!builtProject) process.exitCode = -1;
 
 console.log("Ready!");
